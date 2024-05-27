5eba46a2353a46d2901ff7ca9c5dda1684110a20
sindhuja110 <sindhuja1106@gmail.com>
2024-05-27 16:25:10 +0530

    remove unwanted code

--- lib/Screens/widgets/BottomNavigationScreen.dart
+++ lib/Screens/widgets/BottomNavigationScreen.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:meet_in_ground/Screens/Favorites/Favorites.dart';
 import 'package:meet_in_ground/Screens/Home/home.dart';
