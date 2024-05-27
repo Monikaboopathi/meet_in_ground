e20baed21c311ebcdb4ee55c883cd10b0de46576
sindhuja110 <sindhuja1106@gmail.com>
2024-05-27 18:33:17 +0530

    authenticate for first time and go to home page

---
+++ lib/Screens/util/Services/Auth_service.dart
@@ -0,0 +1,20 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class AuthService {
+  static const String tokenKey = 'jwt_token';
+
+  static Future<void> saveToken(String token) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setString(tokenKey, token);
+  }
+
+  static Future<String?> getToken() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getString(tokenKey);
+  }
+
+  static Future<void> clearToken() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.remove(tokenKey);
+  }
+}
