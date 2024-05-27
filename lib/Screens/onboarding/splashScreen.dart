d9bd33bdcfb5e25d6d2663ccd345649e30a1097f
sindhuja110 <sindhuja1106@gmail.com>
2024-05-27 15:46:37 +0530

    complete the splash screen and onboarding screen

--- lib/Screens/onboarding/splashScreen.dart
+++ lib/Screens/onboarding/splashScreen.dart
@@ -1,1 +1,48 @@
-// ignore_for_file: file_names
+import 'package:flutter/material.dart';
+import 'package:meet_in_ground/Screens/onboarding/Onboarding.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+    _navigateToOnboarding();
+  }
+
+  Future<void> _navigateToOnboarding() async {
+    await Future.delayed(const Duration(seconds: 3));
+    if (!mounted) return;
+
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(
+        builder: (context) => OnboardingScreen(),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        // ignore: sized_box_for_whitespace
+        child: Container(
+          width: double.infinity,
+          height: double.infinity,
+          child: Image.asset(
+            'assets/splash.png',
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+    );
+  }
+}
