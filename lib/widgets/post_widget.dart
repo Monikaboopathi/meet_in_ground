d582497c89f5805d11145a81f29fa699a5b60668
Ranjithkumar7200 <ranjithkumarb8072@gmail.com>
2024-05-28 14:40:29 +0530

    completed dashboard

--- lib/widgets/post_widget.dart
+++ lib/widgets/post_widget.dart
@@ -20,6 +20,15 @@ class Post_Widget extends StatelessWidget {
   final String postOwnerImage;
   final int likes;
   final int comments;
+  final bool isShowMore;
+  final Function(String) onToggleShowMore;
+  final bool isFavorite;
+  final VoidCallback onDeleteFav;
+  final VoidCallback onFavoriteToggle;
+  final bool isRequest;
+  final VoidCallback onDeleteRequest;
+  final VoidCallback onRequestToggle;
+  final currentMobileNumber;
 
   const Post_Widget({
     required this.id,
@@ -37,6 +46,15 @@ class Post_Widget extends StatelessWidget {
     required this.comments,
     Key? key,
     required createdAt,
+    required this.isShowMore,
+    required this.onToggleShowMore,
+    required this.isFavorite,
+    required this.onDeleteFav,
+    required this.onFavoriteToggle,
+    required this.isRequest,
+    required this.onDeleteRequest,
+    required this.onRequestToggle,
+    this.currentMobileNumber,
   }) : super(key: key);
 
   @override
@@ -63,27 +81,31 @@ class Post_Widget extends StatelessWidget {
                   ),
                 ),
                 SizedBox(
-                  width: 5,
+                  width: 8,
                 ),
-                Container(
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                      color: ThemeService.textColor,
-                      width: 1.0,
-                    ),
-                    borderRadius: BorderRadius.circular(4.0),
-                  ),
-                  padding: EdgeInsets.symmetric(vertical: 1, horizontal: 6),
-                  child: Row(
-                    children: [
-                      Text(
-                        "Request",
-                        style: TextStyle(
-                            fontSize: 10.0, color: ThemeService.textColor),
+                if (phoneNumber != currentMobileNumber)
+                  GestureDetector(
+                    onTap: !isRequest ? onRequestToggle : onDeleteRequest,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: ThemeService.textColor,
+                          width: 1.0,
+                        ),
+                        borderRadius: BorderRadius.circular(4.0),
                       ),
-                    ],
-                  ),
-                )
+                      padding: EdgeInsets.symmetric(vertical: 1, horizontal: 6),
+                      child: Row(
+                        children: [
+                          Text(
+                            isRequest ? "Requested" : "Request",
+                            style: TextStyle(
+                                fontSize: 10.0, color: ThemeService.textColor),
+                          ),
+                        ],
+                      ),
+                    ),
+                  )
               ],
             ),
             subtitle: Row(
@@ -132,15 +154,23 @@ class Post_Widget extends StatelessWidget {
           SizedBox(height: 8.0),
           Row(
             children: [
-              IconButton(
-                icon:
-                    Icon(Icons.favorite_border, color: ThemeService.textColor),
-                onPressed: () {},
-              ),
-              IconButton(
-                icon: Icon(Icons.comment, color: ThemeService.textColor),
-                onPressed: () {},
-              ),
+              if (phoneNumber != currentMobileNumber)
+                IconButton(
+                  icon: Icon(
+                    isFavorite ? Icons.favorite : Icons.favorite_border,
+                    color: isFavorite ? Colors.red : ThemeService.textColor,
+                  ),
+                  onPressed: !isFavorite ? onFavoriteToggle : onDeleteFav,
+                ),
+              // IconButton(
+              //   icon: Icon(Icons.delete),
+              //   onPressed: onDeleteFav,
+              // ),
+              if (phoneNumber != currentMobileNumber)
+                IconButton(
+                  icon: Icon(Icons.comment, color: ThemeService.textColor),
+                  onPressed: () {},
+                ),
               IconButton(
                 icon: Icon(Icons.share, color: ThemeService.textColor),
                 onPressed: () {
@@ -171,64 +201,48 @@ class Post_Widget extends StatelessWidget {
               // ),
             ],
           ),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 12.0),
-            child: Text(
-              '$likes likes',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                color: ThemeService.textColor,
+          if (phoneNumber != currentMobileNumber)
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 12.0),
+              child: Text(
+                '$likes like${likes > 1 ? "s" : ""}',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: ThemeService.textColor,
+                ),
               ),
             ),
-          ),
           SizedBox(height: 4.0),
           Padding(
             padding: EdgeInsets.symmetric(horizontal: 12.0),
             child: Text(
-              'Caption',
-              style: TextStyle(
-                fontWeight: FontWeight.w500,
-                color: ThemeService.textColor,
-              ),
-            ),
-          ),
-          SizedBox(height: 8.0),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 12.0),
-            child: Text(
-              'View all $comments comments',
+              isShowMore
+                  ? '${matchDetails}'
+                  : matchDetails.length > 80
+                      ? '${matchDetails.substring(0, 80)}... '
+                      : '${matchDetails}',
               style: TextStyle(color: Colors.grey),
             ),
           ),
           SizedBox(height: 8.0),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 12.0),
-            child: Row(
-              children: [
-                CircleAvatar(
-                  radius: 12.0,
-                  backgroundImage: AssetImage('assets/galleryImage.png'),
-                ),
-                SizedBox(width: 8.0),
-                Text(
-                  'comment',
-                  style: TextStyle(
-                    fontWeight: FontWeight.w500,
-                    color: ThemeService.textColor,
+          matchDetails.length > 80
+              ? Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 12.0),
+                  child: GestureDetector(
+                    onTap: () => onToggleShowMore(id),
+                    child: Text(
+                      isShowMore ? 'Show less' : 'Show more',
+                      style: TextStyle(color: Colors.blue),
+                    ),
                   ),
-                ),
-              ],
-            ),
+                )
+              : Container(),
+          SizedBox(height: 8.0),
+          Divider(
+            color: ThemeService.textColor.withOpacity(0.3),
+            height: 1,
           ),
           SizedBox(height: 8.0),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 12.0),
-            child: Text(
-              '5 hours ago',
-              style: TextStyle(color: Colors.grey),
-            ),
-          ),
-          SizedBox(height: 16.0),
         ],
       ),
     );
